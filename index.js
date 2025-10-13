7328545be50f83384fce01305f7ef405a2872002
Maria Peña <mariapenatorres3@gmail.com>
2025-10-13 11:41:14 -0600

    removed node modules

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hola Mundo");   
+const express = require('express');
+const app = express();
+
+const pokemon = require('./routes/pokemon');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/pokemon', pokemon);
+
+
+app.get('/', (req, res, next) => {
+ return res.status(200).send("bienvenido al pokedex");
+});
+
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server is running...');
+});
